14c15060cf71bc86d64778d2fc2c524711a83f41
Oscar España <espaaoscar@gmail.com>
2022-04-21 20:59:14 -0500

    feat:  Layout y Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import { LayersClearOutlined } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
-    <Typography variant="h1" color="primary">
-      Hola Mundo
-    </Typography>
+    <Layout>
+      <Typography variant="h1" color="primary">
+        Hola Mundo
+      </Typography>
+    </Layout>
   );
 };
 
