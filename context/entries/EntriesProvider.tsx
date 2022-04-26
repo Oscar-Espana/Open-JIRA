8be32f73fcd4db45ef2ff95bd95067d81f5eb42a
Oscar España <espaaoscar@gmail.com>
2022-04-25 21:57:17 -0500

    feat: interface Entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,12 +1,35 @@
 import { FC, ReactNode, useReducer } from "react";
 import { EntriesContext, entriesReducer } from "./";
+import { Entry } from "../../interfaces";
+import { v4 as uuidv4 } from "uuid";
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description:
+        "Cupidatat et elit eiusmod ipsum ea exercitation elit voluptate esse ad.",
+      createdAt: Date.now(),
+      status: "pending",
+    },
+    {
+      _id: uuidv4(),
+      description: "Magna aute nisi est elit cillum velit do.",
+      createdAt: Date.now() - 450000,
+      status: "in-progress",
+    },
+    {
+      _id: uuidv4(),
+      description:
+        "Tempor cupidatat est ad in laborum velit deserunt consequat commodo culpa fugiat non incididunt velit.",
+      createdAt: Date.now() - 100000,
+      status: "finished",
+    },
+  ],
 };
 
 interface Props {
