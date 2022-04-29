e0148496b7e7385fdeb0a005ffdddaab5c936af5
Oscar España <espaaoscar@gmail.com>
2022-04-28 22:17:17 -0500

    feat: Variables de entorno en Next.js

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  secret?: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "Example" });
+  console.log(process.env);
+  res.status(200).json({ name: "Example", secret: process.env.SECRET });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { Layout } from "../components/layouts";
 import { EntryList, NewEntry } from "../components/ui";
 
 const HomePage: NextPage = () => {
+  console.log("env", process.env.NEXT_PUBLIC_CLIENT_KEY);
+
   return (
     <Layout title="Home - Open Jira">
       <Grid container spacing={2}>
